1e3d335580474fd1e9bf8aded02782969ba5da75
Samisafool <thenerdiestguy@gmail.com>
2023-06-15 22:54:10 +0530

    Release 1.7.0

--- create_release.sh
+++ create_release.sh
@@ -10,8 +10,7 @@ modpack_vers=$(jq -r '.pack_version' < ./conf/1.20/config.json)
 git add .
 git commit -m "Release $modpack_vers" --allow-empty
 
-./setup.py 1.19.4
-./setup.py 1.20
+./runscript.sh
 
 git tag "$modpack_vers"
 git push origin main "$modpack_vers"
