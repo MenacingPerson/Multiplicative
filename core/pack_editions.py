c2359cc41536b85a3ac796ea525ba2873fd896fe
Samisafool <thenerdiestguy@gmail.com>
2023-08-20 12:56:36 +0530

    It works

--- core/pack_editions.py
+++ core/pack_editions.py
@@ -15,6 +15,7 @@ def run_in(modloader: str, func, *args):
             if loader_is_valid(i):
                 run_in(i, func, *args)
         return
+    core.base.chodir()
     for pack_edition_path in glob.glob(f'{modloader}/*'):
         if pack_edition_path == []:
             raise NameError(f'No {modloader} pack editions found!')
@@ -23,19 +24,20 @@ def run_in(modloader: str, func, *args):
         pack['modloader'] = modloader
         pack['edition'] = modloader + '+' + core.base.config['game_version']
         pack['fullver'] = f'{core.base.base_conf["pack_name"]}-{core.base.base_conf["pack_version"]}-{pack["edition"]}'
+        core.packwiz.pw_refresh(pack)
         func(pack, *args)
         core.base.chodir()
 
 
 def run_separately_in_all(func, *args):
     """Run function separately in all editions"""
+    args_all = [arg.replace('_[ml]', '') for arg in args]
+    run_in('all', func, *args_all)
     args_forwarded = {}
     for i in core.packwiz.modloaders:
         if loader_is_valid(i):
             args_forwarded[i] = [arg.replace('[ml]', i) for arg in args]
             run_in(i, func, *args_forwarded[i])
-    args = [arg.replace('_[ml]', '') for arg in args]
-    run_in('all', func, *args)
 
 
 def loader_is_valid(modloader: str) -> bool:
