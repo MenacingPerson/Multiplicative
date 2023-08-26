e2291dad2d6efac718e24018fb6cd80c387a682b
Menacin <thenerdiestguy@gmail.com>
2023-08-26 19:58:32 +0530

    Unsup support (#3)

--- scripts/runscript.sh
+++ scripts/runscript.sh
@@ -5,13 +5,18 @@ set -e
 cd "$(realpath "$(dirname "$0")"/..)"
 
 rm -f ./packs/*.mrpack
+rm -rf ./Final_pack
+mkdir Final_pack
 
 for i in $(jq -r '.build[].mc' < conf/base_config.json)
 do
     if [[ -d ./conf/$i ]]
     then
         script -eq -c "./setup.py $i" /dev/null
+        cp Modified/versions ./Final_pack/ -r
     fi
 done
 
 ./scripts/gen_mod_list.sh > mod-list.md
+
+cp Modified/LICENSE Final_pack
