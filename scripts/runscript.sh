9e8ecf0ad73382ba0d82aaf7471186141aeffa2c
Samisafool <thenerdiestguy@gmail.com>
2023-08-26 19:57:26 +0530

    Am I stupid or what

--- scripts/runscript.sh
+++ scripts/runscript.sh
@@ -5,17 +5,18 @@ set -e
 cd "$(realpath "$(dirname "$0")"/..)"
 
 rm -f ./packs/*.mrpack
+rm -rf ./Final_pack
+mkdir Final_pack
 
 for i in $(jq -r '.build[].mc' < conf/base_config.json)
 do
     if [[ -d ./conf/$i ]]
     then
         script -eq -c "./setup.py $i" /dev/null
+        cp Modified/versions ./Final_pack/ -r
     fi
 done
 
 ./scripts/gen_mod_list.sh > mod-list.md
 
-rm -rf ./Final_pack
-cp Modified ./Final_pack -r
-rm -r ./Final_pack/.git* ./Final_pack/README.md ./Final_pack/makefile
+cp Modified/LICENSE Final_pack
