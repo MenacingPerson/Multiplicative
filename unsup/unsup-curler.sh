754c89ea07efa9126a0656c88ae7088aa971693a
Samisafool <thenerdiestguy@gmail.com>
2023-08-29 18:59:31 +0530

    Proper unsup curler

--- unsup/unsup-curler.sh
+++ unsup/unsup-curler.sh
@@ -9,20 +9,27 @@ unsup_url="https://git.sleeping.town/attachments/7edb17a2-e43f-4789-8bae-6140cbe
 
 mkdir -p .cache
 
-for ver in $@
-do
-    cd $ver
+read -p "Modloader: " modloader
 
-    if [[ ! -f ../.cache/unsup-$unsup_ver.jar ]]
-    then
-        curl "$unsup_url" --output ../.cache/unsup-$unsup_ver.jar
-    fi
+read -p "Minecraft Version: " mc_version
 
-    cp ../.cache/unsup-$unsup_ver.jar unsup.jar
-    packwiz refresh
-    packwiz mr export -o ../../packs/Multiplicative-$ver-unsup-$unsup_ver.mrpack
-    rm unsup.jar
-    packwiz refresh
+mkdir unsup-$modloader-$mc_version
+cd unsup-$modloader-$mc_version
 
-    cd ..
-done
+packwiz init --name "Multiplicative Unsup $modloader $mc_version" --author MenacingPerson --mc-version $mc_version \
+--version 1.0.0 --modloader $modloader --$modloader-version $(jq -r ".modloaders.$modloader.version" < ../../conf/base_config.json)
+
+if [[ ! -f ../.cache/unsup-$unsup_ver.jar ]]
+then
+    curl "$unsup_url" --output ../.cache/unsup-$unsup_ver.jar
+fi
+
+cp ../.cache/unsup-$unsup_ver.jar unsup.jar
+
+cp ../unsup.ini .
+
+sed -i "s|mcver|$modloader/$mc_version|g" unsup.ini
+
+packwiz refresh
+
+packwiz mr export -o ../../packs/Multiplicative-$modloader-$mc_version-unsup-$unsup_ver.mrpack
